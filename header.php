1c2a5491c3265b8c1651dcf7bb76ef77df8554d7
Praveena Mihiravi <mihiravipraveena@gmail.com>
2026-03-25 00:10:39 +0530

    header

---
+++ header.php
@@ -0,0 +1,15 @@
+<section class="header">
+    <a href="home.php" class="logo"> Evermore More Events </a>
+
+    <nav class="navbar">
+        <a href="home.php">Home</a>
+        <a href="about.php">About</a>
+        <a href="poortrait.php">Portrait</a>
+        <a href="services.php">Services</a>
+        <a href="contact.php">Contact</a>
+
+    </nav> 
+    
+    <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
